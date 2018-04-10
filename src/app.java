114d191ca098880877544d65afcb97d5434c2553
pczao <pczao@micrao2016-PC>
2018-04-10 11:56:05 -0300

    tentando enteder o trabalho

--- src/app.java
+++ src/app.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
 
 public class app {
+	
+	private int ts, tf;
+	
+	private int intervalo[] =  new int[2];
+	private ArrayList<Integer> conjunto;
+	
+	private void gerador(){
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("teste");
 	}
 
 }
