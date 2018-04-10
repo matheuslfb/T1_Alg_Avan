2379e41e4a53e8945ef941d0492a7d5b9284b5a6
pczao <pczao@micrao2016-PC>
2018-04-10 14:13:13 -0300

    teste gerador

---
+++ src/Intervalo.java
@@ -0,0 +1,36 @@
+
+public class Intervalo {
+	
+	private int ts, tf;
+
+	public Intervalo() {
+		super();
+		this.ts = ts;
+		this.tf = tf;
+	}
+
+	public int getTs() {
+		return ts;
+	}
+
+	public void setTs(int ts) {
+		this.ts = ts;
+	}
+
+	public int getTf() {
+		return tf;
+	}
+
+	public void setTf(int tf) {
+		this.tf = tf;
+	}
+
+	@Override
+	public String toString() {
+		return "Intervalo [ts=" + ts + ", tf=" + tf + "]";
+	}
+	
+	
+	
+	
+}
--- src/app.java
+++ src/app.java
@@ -1,20 +1,43 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class app {
-	
+
 	private int ts, tf;
-	
-	private int intervalo[] =  new int[2];
-	private ArrayList<Integer> conjunto;
-	
-	private void gerador(){
+
+	private static ArrayList<Intervalo> conjunto = new ArrayList<>();
+	static Random n = new Random();
+
+	private static void gerador() {
+
+		for (int i = 0; i < 999999; i++) {			
+			conjunto.add(i, new Intervalo());
+//			System.out.println(conjunto.get(i).toString());
+		}
 		
 		
+
+		for (int i = 0; i < conjunto.size(); i++) {
+			int num = n.nextInt(1000); // seta o random
+
+			conjunto.get(i).setTs(num); // TS recebe o random
+
+			if (num < conjunto.get(i).getTs()) { // verifica se TS < TF
+				conjunto.get(i).setTf(num); //
+			}
+		}
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("teste");
+
+		 gerador();
+		for (Intervalo x : conjunto) {
+			System.out.println("TS: " + x.getTs());
+			System.out.println("TF: " + x.getTf());
+		}
+
 	}
 
 }
